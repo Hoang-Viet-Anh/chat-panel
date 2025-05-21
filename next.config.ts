4c22d39d02a144ff4bc8bfb2a30074379498dbb9
Hoang-Viet-Anh <xkrules.pro@gmail.com>
2025-05-21 15:02:08 +0300

    Changed next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: []
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   }
 };
 
